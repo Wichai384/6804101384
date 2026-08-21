0caebf76113f68ed0cbe7439153f87a133bc953e
Wichai <MJU6804101384@mju.ac.th>
2026-08-21 13:45:42 +0700

    v.4-Label with Thai language-384

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,8 +1,8 @@
 <template>
   <q-page class="q-pa-md">
     <q-form class="q-gutter-md" style="max-width: 600px">
-      <q-input filled label="Your name *" hint="Name and surname" />
-      <q-input filled type="number" label="Your age *" />
+      <q-input filled label="Your name (ชื่อ-สกุล) *" hint="Name and surname" />
+      <q-input filled type="number" label="Your age (อายุ) *" />
       <q-toggle label="I accept the license and terms" />
       <div>
         <q-btn label="SUBMIT" type="submit" color="primary" />
@@ -13,31 +13,31 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import { useQuasar } from 'quasar'
+  import { ref } from 'vue'
+  import { useQuasar } from 'quasar'
 
-const $q = useQuasar()
-const name = ref('')
-const age = ref(null)
-const accept = ref(false)
+  const $q = useQuasar()
+  const name = ref('')
+  const age = ref(null)
+  const accept = ref(false)
 
-function onSubmit () {
-  if (accept.value !== true) {
-    $q.notify({
-      type: 'negative',
-      message: 'You need to accept the license and terms first'
-    })
-  } else {
-    $q.notify({
-      type: 'positive',
-      message: 'Submitted successfully'
-    })
+  function onSubmit () {
+    if (accept.value !== true) {
+      $q.notify({
+        type: 'negative',
+        message: 'You need to accept the license and terms first'
+      })
+    } else {
+      $q.notify({
+        type: 'positive',
+        message: 'Submitted successfully'
+      })
+    }
   }
-}
 
-function onReset () {
-  name.value = ''
-  age.value = null
-  accept.value = false
-}
+  function onReset () {
+    name.value = ''
+    age.value = null
+    accept.value = false
+  }
 </script>
